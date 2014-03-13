0f15613928d283d3e323abc0e88dca81fba14f40
sherifismail <sherif_diaa@aucegypt.edu>
2014-03-13 17:01:37 +0200

    added beq instruction

--- instructions.py
+++ instructions.py
@@ -190,6 +190,13 @@ class addi(instruction):
 		text_segment_instance.globl_main[text_segment_instance.pc][2].value = text_segment_instance.globl_main[text_segment_instance.pc][1].value & \
 		text_segment_instance.globl_main[text_segment_instance.pc][3]
 		return text_segment_instance.globl_main[text_segment_instance.pc][2].value
+class beq(instruction):
+	def __init__(self):
+		instruction.__init__(self)
+		self.opcode = 0x4
+		self.name = 'beq'
+	def execute(self):
+		pass
 class syscall(instruction):
 	def __init__(self):
 		instruction.__init__(self)
@@ -200,5 +207,5 @@ class syscall(instruction):
 		pass
 class instruction_index:
 	def __init__(self):
-		self.instruction_op_index = {0xf : lui(), 0xd : ori(), 0x8 : addi(), (0x0, 0x20) : add(), 0x9  : addiu(), 0x23 : lw(), 0x5 : bne(), (0x0, 0xc) : syscall(), (0x0, 0x22) : sub(), (0x0,0x24):and_(), 0xa: slti(), (0x0, 0x25) : or_(), (0x0, 0x2a) : slt(), (0x0, 0x23) : subu(), (0x0, 0x21) : addu(), (0x0, 0x26) : xor(), (0x0, 0x00) : sll(), (0x0, 0x02) : srl(), 0xe  : xori(), 0xc  : addi()}
+		self.instruction_op_index = {0xf : lui(), 0xd : ori(), 0x8 : addi(), (0x0, 0x20) : add(), 0x9  : addiu(), 0x23 : lw(), 0x5 : bne(), (0x0, 0xc) : syscall(), (0x0, 0x22) : sub(), (0x0,0x24):and_(), 0xa: slti(), (0x0, 0x25) : or_(), (0x0, 0x2a) : slt(), (0x0, 0x23) : subu(), (0x0, 0x21) : addu(), (0x0, 0x26) : xor(), (0x0, 0x00) : sll(), (0x0, 0x02) : srl(), 0xe  : xori(), 0xc  : addi(), 0x4 : beq()}
 instruction_instance = instruction_index()
